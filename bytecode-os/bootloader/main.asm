b046f710700b3eb1bc54971bf977f2ceded36423
Jan Allersma <jan.allersma@lable.nl>
2023-01-16 20:35:54 +0100

    Working first attempt at non-bios print

--- bytecode-os/bootloader/main.asm
+++ bytecode-os/bootloader/main.asm
@@ -1,55 +1,117 @@
 ; Te compileren met NASM: nasm -f bin -o allersmaboot.bin main.asm
+; Gespiekt vanaf https://wiki.osdev.org/Babystep4
+; TODO: na deze code gecombineerd te hebben met implementatie op master, https://wiki.osdev.org/Babystep5
+; NOTE TO SELF: enter is eigenlijk (\r = 10 met \n = 13) 
 BITS 16
 
 start:
-	; Define Stack Space
-	mov ax, 07C0h	; De initial location van deze bootloader (ax = 0x7C00 / BITS).
-	add ax, 20h	; Einde bootloader data (ds).
-	mov ss, ax	; ss = Stack Space Location. Hier begint de stack.
-	mov sp, 4096	; Stack Pointer. Werkt achterstevoren; begint dus bij einde.
+       ; Define Stack Space
+       mov ax, 07C0h   ; De initial location van deze bootloader (ax = 0x7C00 / BITS).
+       add ax, 20h     ; Einde bootloader data (ds).
+       mov ss, ax      ; ss = Stack Space Location. Hier begint de stack.
+       mov sp, 4096    ; Stack Pointer. Werkt achterstevoren; begint dus bij einde.
 
-	; Define Data Space
-	mov ax, 07C0h
-	mov ds, ax	; ds = Data Space Location. Hier begint de data.
+       ; Define Data Space
+       mov ax, 07C0h
+       mov ds, ax      ; ds = Data Space Location. Hier begint de data.
 
-	; Print string
-	mov si, message	; si = Source Index register.
-	call print
-	call read
-	cli		; Disable External interrupts
-	hlt		; Halt CPU.
+       cld
+
+       mov ax, 0xb800
+       mov es, ax
+
+       ; Print string
+       mov si, message ; si = Source Index register.
+       call sprint
+       cli             ; Disable External interrupts
+       hlt             ; Halt CPU.
+
+;start:
+;[ORG 0x7c00]      ; add to offsets
+;   	xor ax, ax    ; make it zero
+;   	mov ds, ax   ; DS=0
+;   	mov ss, ax   ; stack starts at 0
+;   	mov sp, 0x9c00   ; 2000h past code start;
+;
+;   	cld
+;
+;   	mov ax, 0xb800   ; text video memory
+;   	mov es, ax
+;
+;   	mov si, message   ; show text string
+;	call sprint
+;
+;	mov ax, 0xb800   ; look at video mem
+;   	mov gs, ax
+;  	mov bx, 0x0000   ; 'W'=57 attrib=0F
+;   	mov ax, [gs:bx]
+;
+;   	mov  word [reg16], ax ;look at register
+;   	call printreg16
+;
+;hang:
+;	jmp hang
 
 data:
-	message db 'Bootloader booted.',0
-	nlmsg db 'Pressed ', 39, 'a', 39, '!',0
+    message db 'Jootloader booted.',0
+    nlmsg db 'Pressed ', 39, 'a', 39, '!',0
+    xpos db 0
+    ypos db 0
 
-read:
-	mov ah, 0h	; Get char (I guess). It is a follow-up for 'int 16h' and further
-	mov cx, ax	; Set inputted character to cx
-	cmp cx, 'a' ; if (cx == 'a') {
-	je .showmsg ; .showmsg }
-	int 16h		; puts(char)
-	mov ah, 0Eh;call print	; display(char)
-	int 10h		; echo char
-	jmp read	; start over again.
+sprint:
+   lodsb      ; string char to AL
+   cmp al, 0
+   jne cprint   ; else, we're done
+   add byte [ypos], 1   ;down one row
+   mov byte [xpos], 0   ;back to left
+   ret
 
-.showmsg:
-	mov si, nlmsg
-	call print
-	jmp read
+cprint:
+   mov ah, 0x0F   ; attrib = white on black
+   mov cx, ax    ; save char/attribute
+   movzx ax, byte [ypos]
+   mov dx, 160   ; 2 bytes (char/attrib)
+   mul dx      ; for 80 columns
+   movzx bx, byte [xpos]
+   shl bx, 1    ; times 2 to skip attrib
 
-print:
-	mov ah, 0Eh
+   mov di, 0        ; start of video memory
+   add di, ax      ; add y offset
+   add di, bx      ; add x offset
 
-.printchar:
-	lodsb		; Load byte from Source Index & increment it.
-	cmp al, 0	; Compare al with NULL.
-	je .done	; if(cmp al, 0) { .done; }
-	int 10h		; puts(loaded_byte);
-	jmp .printchar
+   mov ax, cx        ; restore char/attribute
+   stosw              ; write char/attribute
+   add byte [xpos], 1  ; advance to right
 
-.done:
-	ret
+   jmp sprint
+
+;printreg16:
+;   mov di, outstr16
+;   mov ax, [reg16]
+;   mov si, hexstr
+;   mov cx, 4   ;four places
+;hexloop:
+;   rol ax, 4   ;leftmost will
+;   mov bx, ax   ; become
+;   and bx, 0x0f   ; rightmost
+;   mov bl, [si + bx];index into hexstr
+;   mov [di], bl
+;   inc di
+;   dec cx
+;   jnz hexloop
+;
+;   mov si, outstr16
+;   call sprint
+;
+;   ret
+;
+;hexstr   db '0123456789ABCDEF'
+;outstr16   db '0000', 0  ;register value string
+;reg16   dw    0  ; pass values to printreg16
+;message db 'Jootloader booted.',0
+;nlmsg db 'Pressed ', 39, 'a', 39, '!',0
+;xpos db 0
+;ypos db 0
 
 times 510-($-$$) db 0	; Fill 510 bytes with this bootloader.
 dw 0xAA55		; Bootloader signature: EOF & Geeft aan dat dit een bootloader is.
